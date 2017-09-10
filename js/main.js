011e16bfc99a23e0584a2d7b18ae13dee0279963
Romona Blackwood <romonablackwood@Romonas-MacBook-Pro.local>
2017-09-10 16:09:06 -0700

    First Commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//lesson 1
+// //print hello world to the console
+// console.log("hello world");
+// //print hello world to the page (AKA the document)
+// document.write("hello world")
+// // prints message in pop-up box
+// alert("Kiss me")
+//
+// lesson2
+var name = prompt("Whats your Name?")
+console.log(name)
+
+// lesson3
+var one = 12 + 18
+console.log
+var two = true
+
+var three = "let the sunshine"
+
+var four = [33, 20, 22]
